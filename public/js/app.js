ed6b40e91a0e3a4bc912cc0beedb67f1baa08e4b
dieDeMiguel <diedemiguel@gmail.com>
2020-03-28 08:34:25 -0300

    remove console.log in js/app.js

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,4 @@
 
-console.log('Client side javaScript is loaded');
-
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
